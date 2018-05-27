1db775867696c0eaca4aed2665958b9679c93aa6
Bob Kerns <rwk@acm.org>
2018-05-27 09:33:40 -0700

    Add utility for creating neo4J DateTime objects.

---
+++ tools/neo4j-date.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018 Bob Kerns.
+ */
+
+const {types: {DateTime}} = require('neo4j-driver').v1;
+
+function convertDateTime(date) {
+    if (date instanceof Date) {
+        // All set
+    } else if ((typeof date === 'string') || (typeof date == 'number')) {
+        date = new Date(date);
+    } else {
+        throw new TypeError(`Incorrect type in convertDateTime(${date})`);
+    }
+    return new DateTime(
+        date.getUTCFullYear(),
+        date.getUTCMonth(),
+        date.getUTCDate(),
+        date.getUTCHours(),
+        date.getUTCMinutes(),
+        date.getUTCSeconds(),
+        date.getUTCMilliseconds() * 1000, // nanoseconds
+        0 // TZ
+    );
+}
+
+module.exports = {
+    convertDateTime
+};
