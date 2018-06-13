9a7590cba74b37caaa2d4162e863561d102ecf10
Bob Kerns <rwk@acm.org>
2018-06-13 07:04:56 -0700

    Query expansion and tests.

--- tools/database/query-parser.ts
+++ tools/database/query-parser.ts
@@ -2,12 +2,27 @@
  * Copyright (c) 2018 Bob Kerns.
  */
 
+/**
+ * Parse a query with substitutions of the form `$[PARAMETER]`, and produce an expansion.
+ *
+ * This is for queries which need to be dynamically constructed and can't be handled by the underlying DB's parameter
+ * substitution.
+ *
+ * [[QueryParser.expand]] performs substitutions, and can be used in two ways:
+ * * Expansion as a template, in which case the returned `missing` and `unused` values can be used in reporting
+ *   mismatches with the parameters, and
+ * * Expansion prior to delivery of the query to the underlying database, in which case the returned set of parameters
+ *   are the ones not used in the expansion, and should be sent for substitution by the database.
+ */
 import {future, Future} from "../util/future";
 import {AnyParams} from "../util/types";
 import defineProperty = Reflect.defineProperty;
+import ownKeys = Reflect.ownKeys;
 
-type Pick = (params: AnyParams) => string | undefined;
-type ParseStep = string | Pick;
+import * as R from "ramda";
+
+type Pick = (params: AnyParams) => (string | undefined);
+type ParseStep = string | {param: string, pick: Pick};
 
 interface ParseResult {
     steps: ParseStep[];
@@ -17,11 +32,15 @@ interface ParseResult {
 /**
  * Return result after expansion.
  */
-export interface ExpandResult {
+interface ExpandResult {
     /** The expanded result. */
     statement: string;
     /** Unused supplied parameter values, to be supplied for substitution in running the query. */
     parameters: AnyParams;
+    /** Parameters not supplied, but used in the template */
+    missing: string[];
+    /** Parameters supplied but not used, for error reporting */
+    unused: string[];
 }
 
 export class QueryParser {
@@ -40,28 +59,31 @@ export class QueryParser {
     pick(paramName: string): Pick {
         return(params: AnyParams) => {
             if (!params.hasOwnProperty(paramName)) return undefined;
-            let val = params[paramName];
-            if (val === undefined) return undefined;
-
-            if (!this.validate(val)) {
+            let val: any = params[paramName];
+            if (val === undefined) {
+                return undefined;
+            } else if (!this.validate(val)) {
                 throw new Error(`Invalid value for ${paramName}: ${val}`);
             }
-            return val + '';
+            return this.format(val);
         };
     }
 
     private parse(statement: string): ParseResult {
-        let steps = [];
-        let parameters: string[] = []
+        let steps: ParseStep[] = [];
+        let parameters: string[] = [];
+        if (statement === '') {
+            return {steps: [''], parameters: []};
+        }
         while (statement) {
-            let match = /\$\[([a-zA-Z0-9_]+)\]/.exec(statement);
+            let match = /\$\[([a-zA-Z0-9_]+)]/.exec(statement);
             if (match) {
                 let param = match[1];
                 if (match.index > 0) {
                     steps.push(statement.substring(0, match.index));
                 }
                 parameters.push(param);
-                steps.push(this.pick(param));
+                steps.push({param: param, pick: this.pick(param)});
                 statement = statement.substring(match.index + match[0].length);
             } else {
                 steps.push(statement);
@@ -70,11 +92,42 @@ export class QueryParser {
         }
         return {
             steps: steps,
-            parameters: parameters,
+            parameters: R.uniq(parameters),
         };
     }
 
-    validate(val: any) {
+    protected validate(val: any): boolean {
         return true;
+    }
+
+    protected format(val: any) : string {
+        return '' + val;
+    }
+
+    /**
+     * Expand  the query with the supplied parameters. Return the expanded statement, plus information about used and
+     * unused parameters.
+     * @param {AnyParams} params
+     * @returns {ExpandResult}
+     */
+    public expand(params: AnyParams) : ExpandResult {
+        const missing: string[] = [];
+        const parameters = {...params};
+        let process = (step: ParseStep): string => {
+            if (typeof step === "string") {
+                return step;
+            }
+            let val = step.pick(params);
+            if (val === undefined) {
+                missing.push(step.param);
+                return `$[${step.param}]`;
+            } else {
+                delete parameters[step.param];
+                return val;
+            }
+        };
+        const statement = this.parsed.steps.map(process).join();
+        const unused = ownKeys(parameters) as string[];
+        return {statement, parameters, unused, missing};
     }
 }
--- tools/database/test/query-parser.test.ts
+++ tools/database/test/query-parser.test.ts
@@ -4,55 +4,73 @@
 
 import {QueryParser} from "../query-parser";
 import * as R from "ramda";
+import ownKeys = Reflect.ownKeys;
 
+type Accum = {[key: string]: true};
 
 describe("Testing query parsing and parameter substitution", () => {
-    [
-        {input: "A random string with no parameters", steps: ["A random string with no parameters"]},
-        {input: "begin/$[PARAM]", steps: ['begin/', ["PARAM"]]},
-        {input: "$[PARAM]/end", steps: [["PARAM"], '/end']},
-        {input: "$[PARAM]", steps: [["PARAM"]]},
-        {input: "begin/$[PARAM]/end", steps: ['begin/', ["PARAM"], '/end']},
-        {input: "begin/$[PARAM1]/mid/$[PARAM2]/end", steps: ["begin/", ["PARAM1"], "/mid/", ["PARAM2"], "/end"]},
-        {input: "begin/$[PARAM1]$[PARAM2]/end", steps: ["begin/", ["PARAM1"], ["PARAM2"], "/end"]},
-        {input: "$[PARAM1]/mid/$[PARAM2]", steps: [["PARAM1"], ["PARAM2"]]},
-    ]
-        .forEach(({input, steps}) => {
-            it(`testing ${input}`, () => {
-                const findParams = (accum: string[], val: string|string[]): [(string|null)[], (string|null)] => {
-                    if (Array.isArray( val)) {
-                        return [[...accum, val[0]], null];
-                    }
-                    return [accum, val];
-                };
-                const [params, nsteps]: [(string|null)[], (string|null)[]] = R.mapAccum(findParams, [], steps);
-                const parser = new QueryParser(input);
-                let result = parser.parsed;
-                nsteps.forEach((str: (string|null), i: number) => {
-                    if (str !== null) {
-                        expect(result.steps[i]).toEqual(nsteps[i]);
-                    }
+    describe('Query parsing', () => {
+        [
+            {input: '', steps: ['']},
+            {input: "A random string with no parameters", steps: ["A random string with no parameters"]},
+            {input: "begin/$[PARAM]", steps: ['begin/', ["PARAM"]]},
+            {input: "$[PARAM]/end", steps: [["PARAM"], '/end']},
+            {input: "$[PARAM]", steps: [["PARAM"]]},
+            {input: "begin/$[PARAM]/end", steps: ['begin/', ["PARAM"], '/end']},
+            {input: "begin/$[PARAM1]/mid/$[PARAM2]/end", steps: ["begin/", ["PARAM1"], "/mid/", ["PARAM2"], "/end"]},
+            {input: "begin/$[PARAM1]$[PARAM2]/end", steps: ["begin/", ["PARAM1"], ["PARAM2"], "/end"]},
+            {input: "$[PARAM1]/mid/$[PARAM2]", steps: [["PARAM1"], "/mid/", ["PARAM2"]]},
+            {input: "$[PARAM1]/mid/$[PARAM1]", steps: [["PARAM1"], "/mid/", ["PARAM1"]]},
+        ]
+            .forEach(({input, steps}) => {
+                it(`parsing ${input}`, () => {
+                    const findParams = (accum: Accum, val: string|string[]): Accum => {
+                        if (Array.isArray(val)) {
+                            return {[val[0]]: true, ...accum};
+                        }
+                        return accum;
+                    };
+                    const accum: Accum = steps.reduce(findParams,  {});
+                    const params = ownKeys(accum);
+                    const parser = new QueryParser(input);
+                    let result = parser.parsed;
+                    steps.forEach((str: (string|[string]), i: number) => {
+                        if (Array.isArray(str)) {
+                            let step = result.steps[i];
+                            if (typeof step === 'object') {
+                                expect(step.param).toEqual(str[0]);
+                            } else {
+                                expect(step).toBeInstanceOf(Object);
+                            }
+                        } else {
+                            expect(result.steps[i]).toBe(str);
+                        }
+                    });
+                    //expect(result.parameters).toEqual(params);
+                    expect(result.parameters.sort()).toEqual(params.sort());
                 });
-                expect(result.parameters).toEqual(params);
             });
-        });
-    it("should parse one parameter query", () => {
-        const oneParameter = "begin/$[PARAM]/end";
-        const parser = new QueryParser(oneParameter);
-        const result = parser.parsed;
-        [0, 2].forEach(i => {
-            expect(result.steps[i]).toEqual(['begin/', null, '/end'][i]);
-        });
-        expect(result.parameters).toEqual(['PARAM']);
     });
 
-    it("should parse multiple parameter query", () => {
-        const oneParameter = "begin/$[PARAM1]/mid/$[PARAM2]/end";
-        const parser = new QueryParser(oneParameter);
-        let result = parser.parsed;
-        [0, 2].forEach(i => {
-            expect(result.steps[i]).toEqual(['begin/', null, '/mid/', null, '/end'][i]);
+    describe("Expansion", () => {
+        [
+            {
+                label: 'simple',
+                input: '$[PARAM1]',
+                data: {PARAM1: 'VAL1'},
+                expect: {statement: 'VAL1'}
+            },
+            {
+                input: 'trivial',
+                expect: {statement: 'trivial'}
+            }
+        ].forEach(({label, input, data, expect: result}) => {
+            it(`expanding ${label || input}`, () => {
+                let expected = {statement: input, parameters: {}, missing: [], unused: [], ...result};
+                let query = new QueryParser(input);
+                let actual = query.expand(data || {});
+                expect(actual).toEqual(expected);
+            });
         });
-        expect(result.parameters).toEqual(['PARAM1', 'PARAM2']);
     });
 })
