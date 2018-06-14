8f931344a2429584e9f5028c5f9667a46137bfc9
Bob Kerns <rwk@acm.org>
2018-06-13 20:17:26 -0700

    A bit more doc.

--- tools/neo4j/neo4j.ts
+++ tools/neo4j/neo4j.ts
@@ -6,6 +6,12 @@
  * Kludge to work around the Neo4J driver not co-locating their type and lib files.
  */
 
+/**
+ * Nothing...
+ * @type {null}
+ */
+const header = null;
+
 import Integer from "./neo4j/integer";
 import {NumberOrInteger} from "./neo4j/graph-types";
 
@@ -20,11 +26,20 @@ export * from './neo4j/integer';
 export {default as Integer} from './neo4j/integer';
 export * from './neo4j/result-summary';
 import ResultSummary from './neo4j/result-summary';
+/**
+ * Re-export the ResultSummary type.
+ */
 export type ResultSummary<S extends NumberOrInteger> = ResultSummary<S>;
 export * from './neo4j/result';
 import Result from './neo4j/result';
+/**
+ * Re-export the Result type.
+ */
 export type Result = Result;
 export * from './neo4j/session';
+/**
+ * Re-export the Session type.
+ */
 export {default as Session} from './neo4j/session';
 export * from './neo4j/spatial-types';
 export * from './neo4j/temporal-types';
