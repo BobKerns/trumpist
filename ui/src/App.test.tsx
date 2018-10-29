84beb3bba0db5811bdb0f5449619136ae5827f2b
Bob Kerns <rwk@acm.org>
2018-10-29 11:21:27 -0700

    Make the App test compile again.

--- ui/src/App.test.tsx
+++ ui/src/App.test.tsx
@@ -1,14 +1,17 @@
 import * as ReactDOM from 'react-dom';
 import App from "./App";
 import * as React from 'react';
-import {INode} from "./Node";
+import {INode, ILink} from "./store";
 import {Map} from 'immutable';
-import {ILink} from "./Link";
+import createBrowserHistory from "history/createBrowserHistory";
+
+const history = createBrowserHistory();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
       <App title="testttt"
+           history={history}
            nodes={Map<string, INode>()}
            links={Map<string, ILink>()}
            start="testttt"
