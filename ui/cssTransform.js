af9798fab6ea02516aec5fb596fb5399f5b42251
Bob Kerns <rwk@acm.org>
2018-11-03 22:40:06 -0700

    Improve the build configuration and take control of webpack. This gets the source maps correct and makes debugging livable. May want to make use of new TS --build features speed things up.

---
+++ ui/cssTransform.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018 Bob Kerns.
+ */
+
+module.exports = {
+    process() {
+        return 'module.exports = {};';
+    },
+    getCacheKey() {
+        // The output is always the same.
+        return 'cssTransform';
+    },
+};
